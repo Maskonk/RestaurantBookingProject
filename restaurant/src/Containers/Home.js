c1b1b93f07569a83d0d7b903425e1034288a553c
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-08-18 14:09:40 +0100

    Fixed typo in folder name

---
+++ restaurant/src/Containers/Home.js
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <p> Welcome to Kyle Butchers shop!! </p>
+            </Fragment>
+        )
+    }
+}
+
+export default Home;
