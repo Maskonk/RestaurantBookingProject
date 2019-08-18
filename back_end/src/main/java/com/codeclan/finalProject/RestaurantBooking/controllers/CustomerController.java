8cca752a199251af2db72afd89b158e83dbba1be
Rory <ruaidhiri@gmail.com>
2019-08-18 14:04:45 +0100

    Created derived query to return all customers sorted by number of visits

---
+++ back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/controllers/CustomerController.java
@@ -0,0 +1,24 @@
+package com.codeclan.finalProject.RestaurantBooking.controllers;
+
+import com.codeclan.finalProject.RestaurantBooking.models.Customer;
+import com.codeclan.finalProject.RestaurantBooking.repositories.CustomerRepository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/by-visits-desc")
+    public List<Customer> customersByVisits() {
+        return customerRepository.findByOrderByNoOfVisitsDesc();
+    }
+
+}
--- back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/repositories/CustomerRepository/CustomerRepository.java
+++ back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/repositories/CustomerRepository/CustomerRepository.java
@@ -4,7 +4,12 @@ import com.codeclan.finalProject.RestaurantBooking.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByOrderByNoOfVisitsDesc();
+
 
 }
