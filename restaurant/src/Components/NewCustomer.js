f53e234ae3e188803320338b8f75edfbf2e111df
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-19 14:11:03 +0100

    redundant line of code removed that was causing a warning.

--- restaurant/src/Components/NewCustomer.js
+++ restaurant/src/Components/NewCustomer.js
@@ -5,7 +5,6 @@ const NewCustomer = (props) => (
  <div>
    <h4>Add Details of New Customer Below</h4>
    <CustomerForm onCustomerSubmit={props.onCustomerSubmit}/>
-   <p>{props.onCustomerSubmit}</p>
  </div>
 );
 
