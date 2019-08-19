6f75839050ad271f49c427f7651203be8dd628bb
Patryk <patryksmacki@googlemail.com>
2019-08-19 10:44:36 +0100

    All bookings table

--- restaurant/src/Components/AllBookings.js
+++ restaurant/src/Components/AllBookings.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AllBookingsTable from "./AllBookingsTable";
 
 const AllBooking = () => (
  <div>
    <h4>Display all Booking</h4>
-
+   <AllBookingsTable/>
  </div>
 );
 
