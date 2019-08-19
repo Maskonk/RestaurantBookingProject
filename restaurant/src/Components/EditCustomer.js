faa5a0a7432f6898782ddee8fa51ac984c9fa3cf
Kyle Walker <Randomer44@hotmail.com>
2019-08-19 14:53:19 +0100

    Edit customer form written

--- restaurant/src/Components/EditCustomer.js
+++ restaurant/src/Components/EditCustomer.js
@@ -1,27 +1,56 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 
-const EditCustomer = ({customer}) => {
-    if (!customer) {
-        return <p> Not found </p>
+class EditCustomer extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editable: false
+        };
+        this.handleEdit = this.handleEdit.bind(this);
     }
-    return (
-        <Fragment>
-            <table>
-                <tbody>
+
+
+    render() {
+        if (!this.props.customer) {
+            return <p> Not found </p>
+        }
+
+        return (
+            <Fragment>
+                <table>
+                    <tbody>
                     <tr>
                         <th>Name</th>
                         <th>Phone Number</th>
                         <th>Number of visits</th>
                     </tr>
-                <tr>
-                    <td>{customer.name}</td>
-                    <td>{customer.contact}</td>
-                    <td>{customer.noOfVisits}</td>
-                </tr>
-                </tbody>
-            </table>
-        </Fragment>
-    )
+                    <tr>
+                        <td>{this.props.customer.name}</td>
+                        <td>{this.props.customer.contact}</td>
+                        <td>{this.props.customer.noOfVisits}</td>
+                        <td><button onClick={this.handleEdit}>Edit</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+                { this.state.editable ?
+                    <div> <br />
+                        <h4> Edit customer </h4>
+                    <form>
+                        <label htmlFor="name"> Name: </label>
+                        <input type="text" value={this.props.customer.name} className="name"/> <br />
+                        <label htmlFor="contact"> Phone number: </label>
+                        <input type="text" value={this.props.customer.contact} className="contact"/> <br/>
+                        <input type="submit" value="Save"/>
+                    </form> </div> : null
+                }
+            </Fragment>
+        )
+    }
+
+    handleEdit() {
+        this.setState({editable: !this.state.editable})
+    }
 };
 
 export default EditCustomer;
