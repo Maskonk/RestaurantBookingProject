610b4f3be8e7153c70e97083c44171947e413420
Kyle Walker <Randomer44@hotmail.com>
2019-08-19 12:50:12 +0100

    Added tbody tags to all bookings

--- restaurant/src/Components/AllBookingsTable.js
+++ restaurant/src/Components/AllBookingsTable.js
@@ -2,21 +2,23 @@ import React, {Fragment} from 'react';
 import AllBookingsTableItem from './AllBookingsTableItem';
 
 const AllBookingsTable = (props) => {
-    return (
-        <Fragment>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Table Nr.</th>
-          <th>Date</th>
-          <th>Time</th>
-          <th>Party Size</th>
-          <th>Comments</th>
-        </tr>
-      </table>
-        </Fragment>
-    )
-  }
+  return (
+      <Fragment>
+        <table>
+          <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Table Nr.</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Party Size</th>
+            <th>Comments</th>
+          </tr>
+          </tbody>
+        </table>
+      </Fragment>
+)
+};
 
 
 export default AllBookingsTable;
