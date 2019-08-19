d65693719bdf3012f67d26e047f6343488aa1dd7
Patryk <patryksmacki@googlemail.com>
2019-08-19 15:39:28 +0100

    fist css added

--- restaurant/src/Components/NewBooking.js
+++ restaurant/src/Components/NewBooking.js
@@ -1,11 +1,20 @@
 import React from "react";
+import plan from '../Pictures/Tables_plan_ready.jpg';
+
 
 const NewBooking = () => (
  <div>
    <h4>Page allowing to create a new booking</h4>
 
+   <center>
+   <img src={plan} width="950" height="550" align="center" border="3"/>
+   </center>
+
+   <h4>Page allowing to create a new booking</h4>
+
+
  </div>
-);
+)
 
 
 export default NewBooking;
