7c4a6490716eb907d53b067354c6a0fa5c83b1bf
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-19 10:48:06 +0100

    Added form inputs. Created state in CustomerForm. Linked form inputs to state but inputs and state cannot be changed.

--- restaurant/src/Components/CustomerForm.js
+++ restaurant/src/Components/CustomerForm.js
@@ -1,9 +1,40 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class CustomerForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      contact: ''
+    };
+  }
+
+
   render() {
     return (
-      <p>I'm a comment form </p>
+      <Fragment>
+        <p>I'm a comment form </p>
+        <form className="customer-form">
+          <label for="name-input">Customer Name:</label>
+          <input
+            type="text"
+            placeholder="Customer Name..."
+            id="name-input"
+            value={this.state.name}
+          />
+          <br/>
+          <label for="tel-number-input">Telephone Number:</label>
+          <input
+            type="text"
+            placeholder="Telephone Number..."
+            value={this.state.contact}
+          />
+          <br/>
+          <input type="submit" value="Add new customer"/>
+
+
+        </form>
+      </Fragment>
     )
   }
 }
