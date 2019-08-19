64529507c83b049580fc794bbd5fe0efcb5b655d
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-19 19:06:57 +0100

    Created new Component, BookingForm and used some test content ro ensure it could be rendered in the NewBooking component.

---
+++ restaurant/src/Components/BookingForm.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react';
+
+class BookingForm extends Component {
+
+
+render() {
+  return(
+    <Fragment>
+      <h1>RENDER TEST</h1>
+
+
+    </Fragment>
+  )
+}
+
+}
+
+export default BookingForm;
--- restaurant/src/Components/NewBooking.js
+++ restaurant/src/Components/NewBooking.js
@@ -1,9 +1,10 @@
 import React from "react";
+import BookingForm from './BookingForm'
 
 const NewBooking = () => (
  <div>
    <h4>Page allowing to create a new booking</h4>
-
+   <BookingForm />
  </div>
 );
 
