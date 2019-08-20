a3a3fad1593f613b2fb9aa35a7d2b2049a32f489
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-20 09:52:43 +0100

    state created in booking form and form inputs linked to state.

--- restaurant/src/Components/BookingForm.js
+++ restaurant/src/Components/BookingForm.js
@@ -2,6 +2,47 @@ import React, {Component, Fragment} from 'react';
 
 class BookingForm extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: "",
+      date: "",
+      partySize: "",
+      comments: "",
+      customer:"",
+      table: ""
+    }
+    this.handlePartySizeChange = this.handlePartySizeChange.bind(this)
+    this.handleTimeChange = this.handleTimeChange.bind(this)
+    this.handleDateChange = this.handleDateChange.bind(this)
+    this.handleCommentsChange = this.handleCommentsChange.bind(this)
+    this.handleCustomerChange = this.handleCustomerChange.bind(this)
+    this.handleTableChange = this.handleTableChange.bind(this)
+  }
+
+  handleTimeChange(event) {
+    this.setState({time: event.target.value})
+  }
+
+  handleDateChange(event) {
+    this.setState({date: event.target.value})
+  }
+
+  handlePartySizeChange(event) {
+    this.setState({partySize: event.target.value})
+  }
+
+  handleCommentsChange(event) {
+    this.setState({comments: event.target.value})
+  }
+
+  handleCustomerChange(event) {
+    this.setState({customer: event.target.value})
+  }
+
+  handleTableChange(event) {
+    this.setState({table: event.target.value})
+  }
 
   render() {
     const customerOptions = this.props.customers.map((customer, index) => {
@@ -27,12 +68,16 @@ class BookingForm extends Component {
             <input
               type="time"
               id="booking-time"
+              value={this.state.time}
+              onChange={this.handleTimeChange}
             />
             <br/>
             <label htmlFor="booking-date">Booking Date: </label>
             <input
               type="date"
               id="booking-date"
+              value={this.state.date}
+              onChange={this.handleDateChange}
             />
             <br/>
             <label htmlFor="booking-size">Booking Size: </label>
@@ -40,6 +85,8 @@ class BookingForm extends Component {
               type="number"
               id="booking-size"
               min="1"
+              value={this.state.partySize}
+              onChange={this.handlePartySizeChange}
             />
             <br/>
 
@@ -48,20 +95,24 @@ class BookingForm extends Component {
               type="text"
               id="booking-comments"
               placeholder="Enter comments here..."
+              value={this.state.comments}
+              onChange={this.handleCommentsChange}
             />
             <br/>
 
             <label htmlFor="customer-selector">Customer: </label>
-            <select id="customer-selector" defaultValue="defaultValue">
-              <option disabled value="default">Choose a Customer...</option>
+            <select id="customer-selector"
+            value={this.state.customer} onChange={this.handleCustomerChange}>
+              <option value="">Choose a Customer...</option>
               {customerOptions}
             </select>
 
             <br/>
 
             <label htmlFor="table-selector">Table: </label>
-            <select id="table-selector" defaultValue="defaultValue">
-              <option disabled value="default">Choose a Table...</option>
+            <select id="table-selector"
+            value={this.state.table} onChange={this.handleTableChange}>
+              <option value="">Choose a Table...</option>
               {tableOptions}
             </select>
 
