7f51ac5045885651ac8b32d6ba121040c495bc38
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-20 16:19:37 +0100

    New derived query written which returns tables with capacity greater than supplied value but not equal to.

--- back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/controllers/TableController.java
+++ back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/controllers/TableController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tables")
-public class TablesController {
+public class TableController {
 
     @Autowired
     TableRepository tableRepository;
--- back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/repositories/TableRepository/TableRepository.java
+++ back_end/src/main/java/com/codeclan/finalProject/RestaurantBooking/repositories/TableRepository/TableRepository.java
@@ -4,6 +4,10 @@ import com.codeclan.finalProject.RestaurantBooking.models.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TableRepository extends JpaRepository<Table, Long> {
+
+    List<Table> findByCapacityGreaterThan(int capacity);
 }
