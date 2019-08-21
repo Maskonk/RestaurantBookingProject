594bf2da97202abbb1fac16f6b358d2d962be9df
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-21 11:38:21 +0100

    Code cleaned up.

--- restaurant/src/Components/BookingForm.js
+++ restaurant/src/Components/BookingForm.js
@@ -10,15 +10,13 @@ class BookingForm extends Component {
       partySize: "",
       comments: "",
       customer:"",
-      table: "",
-      testTables: null
+      tables: null
     }
     this.handlePartySizeChange = this.handlePartySizeChange.bind(this)
     this.handleTimeChange = this.handleTimeChange.bind(this)
     this.handleDateChange = this.handleDateChange.bind(this)
     this.handleCommentsChange = this.handleCommentsChange.bind(this)
     this.handleCustomerChange = this.handleCustomerChange.bind(this)
-    this.handleTableChange = this.handleTableChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleAvailClick = this.handleAvailClick.bind(this)
     this.buildPayLoad = this.buildPayLoad.bind(this)
@@ -32,7 +30,7 @@ class BookingForm extends Component {
         partySize: this.state.partySize,
         comments: this.state.comments,
         customer: this.state.customer,
-        table: "http://localhost:8080/tables/" + this.state.testTables[0].id
+        table: "http://localhost:8080/tables/" + this.state.tables[0].id
       }
     )
   }
@@ -40,7 +38,6 @@ class BookingForm extends Component {
   handleSubmit(event){
     event.preventDefault();
     // TODO: input validation
-    // console.log("payload test:", this.buildPayLoad());
 
     fetch("http://localhost:8080/bookings", {
         method: 'POST',
@@ -55,8 +52,7 @@ class BookingForm extends Component {
       date: "",
       partySize: "",
       comments: "",
-      customer:"",
-      table: ""
+      customer:""
     })
   }
 
@@ -76,10 +72,6 @@ class BookingForm extends Component {
     this.setState({customer: event.target.value})
   }
 
-  handleTableChange(event) {
-    this.setState({table: event.target.value})
-  }
-
   handleCommentsChange(event) {
     this.setState({comments: event.target.value})
   }
@@ -90,7 +82,7 @@ class BookingForm extends Component {
     fetch(url + "tables/bigenough/" + (this.state.partySize -1))
       .then(res => res.json())
       .then(data =>
-        this.setState({testTables: data}))
+        this.setState({tables: data}))
       .catch(err => console.error())
   }
 
@@ -102,81 +94,68 @@ class BookingForm extends Component {
       >{customer.name} ({customer.contact})</option>
     })
 
-    if (this.props.tables != null) {
-      const tableOptions = this.props.tables._embedded.tables.map((table, index) => {
-      return <option
-        value={"http://localhost:8080/tables/" + table.id}
-        key={index}
-        >Table: {table.id}, Capacity: {table.capacity}</option>
-      })
 
+    return(
+      <Fragment>
+        <form className="booking-form" onSubmit={this.handleSubmit}>
 
-      return(
-        <Fragment>
-          <form className="booking-form" onSubmit={this.handleSubmit}>
-
-          <label htmlFor="customer-selector">Customer: </label>
-          <select id="customer-selector"
-          value={this.state.customer} onChange={this.handleCustomerChange}>
-            <option value="">Choose a Customer...</option>
-            {customerOptions}
-          </select>
+        <label htmlFor="customer-selector">Customer: </label>
+        <select id="customer-selector"
+        value={this.state.customer} onChange={this.handleCustomerChange}>
+          <option value="">Choose a Customer...</option>
+          {customerOptions}
+        </select>
+        <br/>
+          <label htmlFor="booking-time">Booking Time: </label>
+          <input
+            type="time"
+            id="booking-time"
+            value={this.state.time}
+            onChange={this.handleTimeChange}
+          />
           <br/>
-            <label htmlFor="booking-time">Booking Time: </label>
-            <input
-              type="time"
-              id="booking-time"
-              value={this.state.time}
-              onChange={this.handleTimeChange}
-            />
-            <br/>
-            <label htmlFor="booking-date">Booking Date: </label>
-            <input
-              type="date"
-              id="booking-date"
-              value={this.state.date}
-              onChange={this.handleDateChange}
-            />
-            <br/>
-            <label htmlFor="booking-size">Booking Size: </label>
-            <input
-              type="number"
-              id="booking-size"
-              min="1"
-              value={this.state.partySize}
-              onChange={this.handlePartySizeChange}
-            />
+          <label htmlFor="booking-date">Booking Date: </label>
+          <input
+            type="date"
+            id="booking-date"
+            value={this.state.date}
+            onChange={this.handleDateChange}
+          />
+          <br/>
+          <label htmlFor="booking-size">Booking Size: </label>
+          <input
+            type="number"
+            id="booking-size"
+            min="1"
+            value={this.state.partySize}
+            onChange={this.handlePartySizeChange}
+          />
 
-            {this.state.partySize &&
-            <input type="button" onClick={this.handleAvailClick} value="Check Availability!"/>
-            }
-            <br/>
+          {this.state.partySize &&
+          <input type="button" onClick={this.handleAvailClick} value="Check Availability!"/>
+          }
+          <br/>
 
-            {(this.state.testTables != null && this.state.testTables.length > 0) &&
+          {(this.state.tables != null && this.state.tables.length > 0) &&
 
-              <div>
-                <label htmlFor="booking-comments">Booking Comments: </label>
-                <input
-                  type="text"
-                  id="booking-comments"
-                  placeholder="Enter comments here..."
-                  value={this.state.comments}
-                  onChange={this.handleCommentsChange}
-                />
+            <div>
+              <label htmlFor="booking-comments">Booking Comments: </label>
+              <input
+                type="text"
+                id="booking-comments"
+                placeholder="Enter comments here..."
+                value={this.state.comments}
+                onChange={this.handleCommentsChange}
+              />
 
-                <input type="submit" value="Make booking"/>
-              </div>
-            }
+              <input type="submit" value="Make booking"/>
+            </div>
+          }
 
-          </form>
+        </form>
 
-        </Fragment>
-      )
-    } else {
-      return (
-        <p>Loading...</p>
-      )
-    }
+      </Fragment>
+    )
   }
 }
 
